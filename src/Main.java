45837420ce54f389b69a553162832101746fb25b
Denis <9150771050@mail.ru>
2022-09-22 09:55:25 +0300

    2-e зменения в коммите новой ветки

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Изменения в новой ветке.");
+        byte nw = 2;
+        System.out.println("Изменения " + nw + " в новой ветке.");
     }
 }
